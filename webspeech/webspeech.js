68f2479cff910c695d53af54b0696648e6c3896c
minamijoyo <minamijoyo@gmail.com>
2014-12-12 16:20:22 +0900

    Add listener event

--- webspeech/webspeech.js
+++ webspeech/webspeech.js
@@ -12,11 +12,7 @@
 
   $('#startButton').click(function(){
     console.log('start');
-
-    $('#startButton').attr('disabled','true');
-    $('#stopButton').attr('disabled','false');
-    $('#stopButton').removeAttr('disabled');
-
+    recFormControl(true);
     rec.lang = $('#selectLang').val();
     console.log(rec.lang);
 
@@ -26,14 +22,53 @@
 
   $('#stopButton').click(function(){
     console.log('stop');
-
-    $('#stopButton').attr('disabled','true');
-    $('#startButton').attr('disabled','false');
-    $('#startButton').removeAttr('disabled');
+    recFormControl(false);
 
     rec.stop();
 
   });
+
+  rec.onstart = function(){
+    $('#messageArea').html("<p>state: start</p>");
+  };
+
+  rec.onend = function(){
+    $('#messageArea').html("<p>state: end</p>");
+    recFormControl(false);
+  };
+
+  rec.onaudiostart = function(){
+    $('#messageArea').html("<p>state: audiostart</p>");
+  };
+
+  rec.onaudioend = function(){
+    $('#messageArea').html("<p>state: audioend</p>");
+  };
+
+  rec.onsoundstart = function(){
+    $('#messageArea').html("<p>state: soundstart</p>");
+  };
+
+  rec.onsoundend = function(){
+    $('#messageArea').html("<p>state: soundend</p>");
+  };
+
+  rec.onspeechstart = function(){
+    $('#messageArea').html("<p>state: speechstart</p>");
+  };
+
+  rec.onspeechend = function(){
+    $('#messageArea').html("<p>state: speechend</p>");
+  };
+
+  rec.onnomatch = function(){
+    $('#messageArea').html("<p>state: nomatch</p>");
+  };
+
+  rec.onerror = function(){
+    $('#messageArea').html("<p>state: error</p>");
+    recFormControl(false);
+  };
 
   rec.onresult = function(e){
     var results = e.results;
@@ -44,6 +79,20 @@
         $('#inputText').val(results[i][0].transcript).addClass('isNotFinal');
       }
     }
+    $('#messageArea').html("<p>state: result</p>");
+
   };
+
+  function recFormControl(start){
+    if(start){
+      $('#startButton').attr('disabled','true');
+      $('#stopButton').attr('disabled','false');
+      $('#stopButton').removeAttr('disabled');
+    }else{
+      $('#stopButton').attr('disabled','true');
+      $('#startButton').attr('disabled','false');
+      $('#startButton').removeAttr('disabled');
+    }
+  }
 
 })(document,jQuery);
